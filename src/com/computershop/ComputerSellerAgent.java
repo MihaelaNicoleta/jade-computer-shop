32f8e7ea3ccdd957cb6496f6f95af027384bffd2
MihaelaNicoleta <raumiha21@gmail.com>
2016-12-27 14:49:24 +0200

    Add behaviours for seller buyer

--- src/com/computershop/ComputerSellerAgent.java
+++ src/com/computershop/ComputerSellerAgent.java
@@ -22,10 +22,13 @@ public class ComputerSellerAgent extends Agent {
 	
 	private final String SERVICE_TYPE = "Computer-Selling";
 	private final String SERVICE_NAME = "JADE-Computer-Trading";
+	private final String NOT_AVAILABLE = "Not-Available";
 	
 	private List<Computer> computers;
 	private Map<AID, AtomicInteger> clients;
 	private ComputerSellerGUI sellerGUI;
+	
+	private ComputerSellerAgent sellerAgent;
 	
 	protected void setup() {
 		clients = new HashMap<AID, AtomicInteger>();
@@ -48,6 +51,10 @@ public class ComputerSellerAgent extends Agent {
 		catch(FIPAException fipaException) {
 			fipaException.printStackTrace();
 		}
+		
+		addBehaviour(new OfferRequestsServer());
+		addBehaviour(new OfferRefusalsServer());
+		addBehaviour(new OfferAcceptanceServer());
 	}
 	
 	protected void takeDown() {
@@ -70,9 +77,128 @@ public class ComputerSellerAgent extends Agent {
 		});
 	}
 	
-	/*add behaviours*/
+	/* parse message to get computer details */
+	private Computer createComputerFromMessage(String message) {
+		String parts[] = message.split(":");		
+		
+		String cpuType = parts[0];
+		String gpuType = parts[1];
+		int ramCapacity = Integer.parseInt(parts[2]);
+		int memCapacity = Integer.parseInt(parts[3]);
+		boolean hasSSD = false;
+		if(parts[4].equalsIgnoreCase("yes")) {
+			 hasSSD = true;
+		}
+		
+		int price = Integer.parseInt(parts[5]);
+		
+		/* create computer with received details*/
+		return new Computer(cpuType, gpuType, ramCapacity, memCapacity, hasSSD, price);
+		
+	}
+	
+	/*add behaviours */
+	
+	/* computer offers --> proposal or refusal*/
+	private class OfferRequestsServer extends CyclicBehaviour {
+		private static final long serialVersionUID = 1L;
+		
+		@Override
+		public void action() {
+			MessageTemplate messageTemplate = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+			ACLMessage aclMessage = sellerAgent.receive(messageTemplate);
+			
+			if(aclMessage != null) {
+				String message = aclMessage.getContent();				
+				Computer computer = createComputerFromMessage(message);
+				
+				/* create reply message */
+				ACLMessage replyMessage = aclMessage.createReply();
+				
+				if(computer != null) {
+					replyMessage.setPerformative(ACLMessage.PROPOSE);
+					replyMessage.setContent(String.valueOf(computer.getPrice()));
+				}
+				else {
+					replyMessage.setPerformative(ACLMessage.REFUSE);
+					replyMessage.setContent(NOT_AVAILABLE);
+				}
+				
+				sellerAgent.send(replyMessage);
+			}
+			else {
+				block();
+			}
+		}		
+	}
 	
 	
+	/* refuse offer */
+	private class OfferRefusalsServer extends CyclicBehaviour {
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public void action() {
+			MessageTemplate messageTemplate = MessageTemplate.MatchPerformative(ACLMessage.REJECT_PROPOSAL);
+			ACLMessage aclMessage = sellerAgent.receive(messageTemplate);
+			
+			if(aclMessage != null) {
+				String message = aclMessage.getContent();
+				Computer computer = createComputerFromMessage(message);
+				
+				if(computer != null) {
+					computer.incrementRefusals();
+					sellerGUI.incrementRefusalsForComputer(computer.getCpuType(), computer.getRamCapacity(), computer.getMemCapacity(), computer.getPrice());
+				}
+			}
+			else {
+				block();
+			}
+		}
+	}
 	
+	/* accept offer */
+	private class OfferAcceptanceServer extends CyclicBehaviour {
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public void action() {
+			MessageTemplate messageTemplate = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);
+			ACLMessage aclMessage = sellerAgent.receive(messageTemplate);
+			
+			if(aclMessage != null) {
+				String message = aclMessage.getContent();
+				Computer computer = createComputerFromMessage(message);
+				
+				ACLMessage replyMessage = aclMessage.createReply();
+				
+				if(computer != null) {
+					/* get client */
+					AID aid = aclMessage.getSender();
+					if(!clients.containsKey(aid)) {
+						clients.put(aid, new AtomicInteger(1));
+					}
+					else {
+						clients.get(aid).incrementAndGet();
+					}
+					
+					/* increment sales */
+					computer.incrementSalesAndDecreaseStock();
+					sellerGUI.incrementSalesAndDecreaseStockForComputer(computer.getCpuType(), computer.getRamCapacity(), computer.getMemCapacity(), computer.getPrice());
+					
+					replyMessage.setPerformative(ACLMessage.INFORM);
+				}
+				else {
+					replyMessage.setPerformative(ACLMessage.FAILURE);
+					replyMessage.setContent(NOT_AVAILABLE);					
+				}
+				
+				sellerAgent.send(replyMessage);
+			}
+			else {
+				block();
+			}
+		}
+	}
 	
 }
