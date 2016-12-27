a895aca681d5b50bfaffe3adafa1d01a56802d64
MihaelaNicoleta <raumiha21@gmail.com>
2016-12-27 13:48:55 +0200

    Add Computer Seller Interface with default methods

---
+++ src/com/computershop/ComputerSellerGUI.java
@@ -0,0 +1,29 @@
+package com.computershop;
+
+import javax.swing.JFrame;
+
+public class ComputerSellerGUI extends JFrame{
+
+	
+	ComputerSellerGUI(ComputerSellerAgent sellerAgent) {
+		
+	}
+	
+	public void show() { 
+		
+	}	
+	
+	public void addComputer(final Computer computer) { 
+		
+	}
+	
+	public void incrementSalesAndDecreaseStockForComputer(final String cpuType, final int ramCapacity, final int memCapacity, final int price) {
+		
+	}
+	
+	public void incrementRefusalsForComputer(final String cpuType, final int ramCapacity, final int memCapacity, final int price) {
+		
+	}
+	
+	
+}
